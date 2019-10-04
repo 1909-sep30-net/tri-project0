580aac1f008e9e542709ece5d40ba9f90c21a5ec
Topsunkist <arizonadub12@gmail.com>
2019-10-04 10:48:59 -0500

    Testing out git on Visual Studio

--- StoreApplication/StoreApplication/Customer.cs
+++ StoreApplication/StoreApplication/Customer.cs
@@ -6,9 +6,23 @@ namespace StoreApplication
 {
     class Customer
     {
+        private string name;
+
+        public Customer() 
+        {
+            string fname;
+            string lname;
+        }
+
 
         public string defaultStore = "Arlington";
-        public string firstName { get; set; }
+        public string firstName
+        {
+            get
+            {
+
+            }
+        }
 
         public string lastName  { get; set; }
         
--- StoreApplication/StoreApplication/Location.cs
+++ StoreApplication/StoreApplication/Location.cs
@@ -10,9 +10,13 @@ namespace StoreApplication
 
         public int inventoryDecrease { get; set; }
 
-        public int fullfillInventory()
+        public int fullfillInventory(int order)
         {
-
+            if(order < inventoryCount)
+            {
+                return 1; //return one to indicate we have enough stock
+            }
+            return -1; //return negative one to say we don't have enough
         }
     }
 }
